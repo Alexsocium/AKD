1c09a1250f9a4a8ce3541c8ed7b77bc1b5046f29
Alexsocium <91365125+Alexsocium@users.noreply.github.com>
2021-09-28 13:45:07 +0300

    /ugadai
    Добавил новый хендлер, нуждается в доработке

--- main.py
+++ main.py
@@ -13,22 +13,34 @@ dp = Dispatcher(bot)
 
 fucups = ['блять', 'пиздец']
 
+@dp.message_handler(commands=['pr'])
+async def prm(message:types.Message):
+    await message.reply('Пиши число дурик')
+    time.sleep(5)
+    print(message.text)
+
+@dp.message_handler(commands=['ugadai'])
+async def process_ugadai_command(message: types.Message):
+    print('fuh')
+    time.sleep(4)
+    a = random.randint(1, 10000)
+    fl = True
+    await message.reply('У тебя есть 5 секунд чтобы написать первое число')
+    while fl:
+        time.sleep(5)
+        if a > int(message.text):
+            await message.reply('Мое число больше')
+        elif a < int(message.text):
+            await message.reply('Мое число меньше')
+        elif a == int(message.text):
+            await message.reply('Поздравляю, вы прошли игру')
+            f1 = False
+
 
 @dp.message_handler(commands=['start'])
 async def psc(message: types.Message):
     await message.reply('Я могу ответить всем и каждому')
 
-@dp.message_handler()
-async def pec(message: types.Message):
-    if message.text == 'Рандомное число':
-
-        await message.reply(str(random.randint(1, 9999)))
-
-    for i in fucups:
-        if i in message.text:
-            await message.reply('Так не красиво')
-            time.sleep(2)
-            await message.delete()
 
 @dp.message_handler(commands=['help'])
 async def phc(message: types.Message):
@@ -36,30 +48,6 @@ async def phc(message: types.Message):
                      '/ugch - угадай число'))
     await message.reply(texti)
 
-@dp.message_handler(commands=['ugadai'])
-async def process_ugch_command(message: types.Message):
-    await message.reply('Выберите сложность')
-    await message.reply('Легкая, Сложная')
-    if message.text == 'Легкая':
-        a = random.randint(1, 500)
-        fl = True
-        while fl:
-            if a > int(message.text):
-                await message.reply('Мое число больше')
-            elif a < int(message.text):
-                await message.reply('Мое число меньше')
-            elif a == int(message.text):
-                await message.reply('Поздравляю, вы прошли игру')
-    elif message.text == 'Сложная':
-        a = random.randint(1, 10000)
-        fl = True
-        while fl:
-            if a > int(message.text):
-                await message.reply('Мое число больше')
-            elif a < int(message.text):
-                await message.reply('Мое число меньше')
-            elif a == int(message.text):
-                await message.reply('Поздравляю, вы прошли игру')
 
 
 
