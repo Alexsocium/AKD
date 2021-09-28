ade7d0e6997ecb2e2b64916460ea0b7c61a91de0
Alexsocium <91365125+Alexsocium@users.noreply.github.com>
2021-09-28 09:19:03 +0300

    upd

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-import random
+import random, time
 
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
+import markaps
 
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+fucups = ['блять', 'пиздец']
+
+
 @dp.message_handler(commands=['start'])
 async def psc(message: types.Message):
-    await message.reply('Я могу ответить всем и каждому')
+    await bot.send_message(message.from_user.id, 'Я могу ответить всем и каждому',
+                           reply_markup = markaps.mainmenu)
 
 @dp.message_handler()
 async def pec(message: types.Message):
-    if message.text == 'Рандомное число':
+    if message.text == 'Рандомное число - 🏹':
 
         await message.reply(random.randint(1, 9999))
+
+    for i in fucups:
+        if i in message.text:
+            await message.reply('Так не красиво')
+            time.sleep(2)
+            await message.delete()
 
 @dp.message_handler(commands=['help'])
 async def phc(message: types.Message):
     await message.reply('gg ne budet /photo')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
 
